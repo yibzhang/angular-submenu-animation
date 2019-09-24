dd180a806781a2f7ca5892017c0af0dfd757853b
yibzhang <zhangyiboturku@gmail.com>
2019-09-24 14:23:40 +0300

    4 files modified, 2 added, 1 deleted, test checking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { SubMenuComponent } from './sub-menu/sub-menu.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, SubMenuComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
